0e58e7cafb0db2bfd8212d88d31b25feddb84fe2
Joe Fitzpatrick <jf@emisense.com>
2012-01-15 10:24:32 -0800

    Removed some build stuff and errant .DS_Store

--- Builds/Android/obj/local/armeabi/objs-debug/juce_jni/__/__/__/JuceLibraryCode/BinaryData.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/juce_jni/__/__/__/JuceLibraryCode/BinaryData.o: \
- jni/../../../JuceLibraryCode/BinaryData.cpp \
- jni/../../../JuceLibraryCode/BinaryData.h
-
-jni/../../../JuceLibraryCode/BinaryData.h:
--- Builds/Android/obj/local/armeabi/objs/juce_jni/__/__/__/JuceLibraryCode/BinaryData.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/juce_jni/__/__/__/JuceLibraryCode/BinaryData.o: \
- jni/../../../JuceLibraryCode/BinaryData.cpp \
- jni/../../../JuceLibraryCode/BinaryData.h
-
-jni/../../../JuceLibraryCode/BinaryData.h:
